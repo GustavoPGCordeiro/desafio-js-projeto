aa2e4d10a6b61ff075ebb37db3661674fc95e131
GustavoPGCordeiro <gustavopgcordeiro@gmail.com>
2025-01-11 14:02:55 -0300

    Add files via upload

---
+++ desafio-js-projeto_inicial (1)/logica-js-projeto_inicial/app.js
@@ -0,0 +1,34 @@
+alert("Olá mundo!");
+alert("Vamos contar até 10?");
+let contador;
+contador = 0
+while (contador < 10){
+    contador++;
+    alert(`${contador}`);
+}
+alert("Até o zero agora?");
+alert("Vamos lá?");
+let variavel;
+variavel = 11;
+while (variavel >= 1){
+    variavel--;
+    alert(`${variavel}`);
+}
+alert("Vamos fazer uma contagem regressiva?");
+let numero;
+numero >= 0;
+numero = prompt ("Escolha um número");
+alert(`${numero}`);
+while( numero >= 1){
+    numero--;
+    alert(`${numero}`)
+}
+alert("Vamos fazer uma contagem progressiva?");
+let numeroG;
+let zero;
+zero = -1;
+numeroG = prompt("Escolha um número");
+while(numeroG > zero){
+    zero++;
+    alert(`${zero}`);
+}
